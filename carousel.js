19ea873ee35c4e96c26aef523d7297feae4187e5
karthik2804 <karthik@karthik.localdomain>
2020-06-08 17:30:11 +0530

    dynamic posotion of thumbnail

--- carousel.js
+++ carousel.js
@@ -4,9 +4,9 @@ class CarouselSlides {
     constructor(currentUrl) {
         this.currentCard = "card0"
         this.updateCard = "card1"
-        this.card0 = el("img.w-100.flex-shrink-0", {src:currentUrl})
-        this.card1 = el("img.w-100.flex-shrink-0")
-        this.el = el("div.w-100.h-100.flex", this.card0, this.card1)
+        this.card0 = el("img.w-100.flex-shrink-0", {style:"height:auto",src:currentUrl})
+        this.card1 = el("img.w-100.flex-shrink-0", {style:"height:auto"})
+        this.el = el("div.w-100.flex", this.card0, this.card1)
         setStyle(this.card0, {order: 0, transition:"transform 1s ease-in-out"})
         setStyle(this.card1, {order: 1, transition:"transform 1s ease-in-out"})
     }
@@ -46,8 +46,8 @@ class Thumbnail {
 }
 
 class Carouselthumbnails {
-    constructor(notifyParent) {
-        this.el = list(el("div.w-100.h3.pa2.flex.justify-center"), Thumbnail, null, [notifyParent])
+    constructor(isVertical, notifyParent) {
+        this.el = list(el("div.flex" + (isVertical ? ".w-100":".w10.flex-column"),{ style:"overflow-x:scroll"}), Thumbnail, null, [notifyParent])
     }
     update(data) {
         this.el.update(data)
@@ -55,27 +55,32 @@ class Carouselthumbnails {
 }
 
 class CarouselContainer {
-    constructor(items) {
+    constructor(isVertical, isThumnailFirst,items) {
+        this.isVertical = isVertical
+        this.isThumnailFirst = isThumnailFirst
         this.items = items
         this.length = items.length
         this.currentItem = 0
+        this.thumbnailVisibile = false
         this.slides = new CarouselSlides(this.items[0])
-        this.totalSlide = el("div.w-100", {onclick:function(e) {
+        this.totalSlide = el("div.w-100", {style:"overflow-x:hidden", onclick:function(e) {
             let width = this.slides.el.getBoundingClientRect().width
-            if (e.clientX < 100 ) {
+            if (e.clientX < width *0.25 ) {
                 this.onChildEvent("prevItem")
             }
-            else if(e.clientX > width - 100) {
+            else if(e.clientX > width * 0.75) {
                 this.onChildEvent("nextItem")
+            }
+            else {
+                this.onChildEvent("togglingThumbnail")
             }
             return
         }.bind(this)}, this.slides)
-        this.thumbnails = new Carouselthumbnails(this.onChildEvent.bind(this))
-        this.el = el("div.w-100.h-100", {style:"overflow-x:hidden"}, this.totalSlide, this.thumbnails)
+        this.thumbnails = new Carouselthumbnails(this.isVertical,this.onChildEvent.bind(this))
+        setStyle(this.thumbnails.el, {order:(this.isThumnailFirst ? -1:1)})
+        this.el = el("div.w-100.h-100.flex" + (this.isVertical ? ".flex-column": ""), {style:"overflow-x:hidden"}, this.totalSlide, this.thumbnails)
         this.thumbnails.update(this.items)
-    }
-    update() {
-
+        setStyle(this.thumbnails.el, {"max-height":"0px", transition:"all 1s ease-in-out 1s"})
     }
     onChildEvent(type, data) {
         switch(type) {
@@ -102,19 +107,30 @@ class CarouselContainer {
             case "moveToItem":
                 if (this.currentItem < data ) {
                     this.slides.update(this.items[data], "right")
-                    this.currentItem = data
                 }
                 else if(this.currentItem > data) {
                     this.slides.update(this.items[data], "left")
-                    this.currentItem = data
                 }
-                return
+                this.currentItem = data
+                setStyle(this.thumbnails.el, {"max-height":"0px"})
+                this.thumbnailVisibile = !this.thumbnailVisibile
+                break
+            case "togglingThumbnail":
+                    console.log(this.thumbnailVisibile)
+                    if (this.thumbnailVisibile) {
+                        setStyle(this.thumbnails.el, {"max-height":"0px"})
+                    }
+                    else {
+                        setStyle(this.thumbnails.el, {"max-height":"500px"})
+                    }
+                    this.thumbnailVisibile = !this.thumbnailVisibile
                 break
         }
+        return
     }
 }
 
-let carousel = new CarouselContainer(["img1.jpeg","img2.jpeg","img3.jpeg"])
+let carousel = new CarouselContainer(true, true, ["img1.jpeg","img2.jpeg","img3.jpeg","img2.jpeg","img3.jpeg","img2.jpeg","img3.jpeg","img2.jpeg","img3.jpeg","img2.jpeg","img3.jpeg","img2.jpeg","img3.jpeg"])
 mount(document.body, carousel)
 
 setStyle(document.body, {margin: 0, padding: 0, width:"100%", height: "100%", "box-sizing": "border-box", "overflow-x": "hidden"})
